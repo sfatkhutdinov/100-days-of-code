cd069f75b33b335e0d01f9d2f62caba6efa2ec93
Stanislav Fatkhutdinov <stanislav.fatkhutdinov@gmail.com>
2021-01-26 10:01:19 -0500

    day 26 list comprehension

---
+++ create_folders.py
@@ -0,0 +1,21 @@
+import os
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+
+
+# for day in range(1, 101):
+#     createFolder(f'./day-{day}/')
+#     with open(f'./day-{day}/day_{day}.py', 'w') as file:
+#         pass       
+
+day = 26 # TODO: Change the date
+createFolder(f'./day-{day}/')
+with open(f'./day-{day}/day_{day}.py', 'w') as file:
+    pass
+# Creates a folder in the current directory called data
+
---
+++ day-26/day_26.py
@@ -0,0 +1,29 @@
+numbers = [1, 2, 3]
+new_list = [n + 1 for n in numbers]
+print(new_list)
+
+name1 = "Stanislav"
+letter_list = [letter for letter in name1]
+print(letter_list)
+
+new_range = [new_range * 2 for new_range in range(1, 5)]
+print(new_range)
+
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
+
+long_names = [name.upper() for name in names if len(name) > 5]
+print(long_names)
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above 👆
+
+#Write your 1 line code 👇 below:
+
+squared_numbers = [number**2 for number in numbers]
+
+#Write your code 👆 above:
+
+print(squared_numbers)
