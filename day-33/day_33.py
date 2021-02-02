dc7cac57d6a2a2aebff4a0611edf5d93e455f43f
Stanislav Fatkhutdinov <stanislav.fatkhutdinov@gmail.com>
2021-02-02 09:15:27 -0500

    day 33 iss tracker complete

--- day-33/day_33.py
+++ day-33/day_33.py
@@ -1,13 +1,33 @@
 import requests
+from datetime import datetime
+MY_LAT = 39.163681
+MY_LNG = -76.708580
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# latitude = data['iss_position']['latitude']
+# longitude = data['iss_position']['longitude']
+#
+# iss_position = (latitude, longitude)
+#
+# print(iss_position)
+
+parameters = {
+    'lat': MY_LAT,
+    'lng': MY_LNG,
+    'formatted': 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
+sunrise = data['results']['sunrise'].split('T')[1].split(':')[0]
+sunset = data['results']['sunset'].split('T')[1].split(':')[0]
+print(sunrise)
+print(sunset)
 
-latitude = data['iss_position']['latitude']
-longitude = data['iss_position']['longitude']
-
-iss_position = (latitude, longitude)
-
-print(iss_position)
+time_now = datetime.now()
+print(time_now.hour)
